3886de11a5f53427e20d1e2d69621a43dd156fd6
kristijan-pajtasev <znanje11$>
2017-04-26 21:37:10 +0100

    randomly adding mines

--- minesweeper.js
+++ minesweeper.js
@@ -1,10 +1,11 @@
 class Minesweeper {
-    constructor(rows = 5, columns = 5, mines = 3) {
+    constructor(rows = 5, columns = 5, mines = 5) {
         this.rows = 3;
         this.columns = 3;
         this.mines = 3;
 
         this.field = this.generateEmptyField(rows, columns);
+        this.addMines(rows, columns, mines, this.field);
     }
 
     generateEmptyField(rows, columns) {
@@ -22,6 +23,18 @@ class Minesweeper {
         return field;
     }
 
+    addMines(rows, columns, numberOfMines, field) {
+        while(numberOfMines > 0) {
+            let row = Math.floor(Math.random() * rows);
+            let column = Math.floor(Math.random() * columns);
+            if(!field[row][column].hasMine) {
+                field[row][column].hasMine = true;
+                numberOfMines--;
+            }
+
+        }
+    }
+
     toString() {
         return this.field.reduce(
             (b,r) => b + r.reduce((a,b) => a + this.getStringRepresentationForPosition(b),"") + "\n", ""
@@ -29,7 +42,7 @@ class Minesweeper {
     }
 
     getStringRepresentationForPosition(field) {
-        return "-";
+        return field.hasMine ? "M" : "-";
     }
 }
 
